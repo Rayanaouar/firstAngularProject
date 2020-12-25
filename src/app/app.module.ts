e01e4ef035be876030a321b025e9ed0862939e6f
Raya Naouar <raya.naouar@episousse.com.tn>
2020-12-25 10:23:45 +0100

    add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {NavbarComponent} from './shared/navbar/navbar.component';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+ 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent
   ],
   imports: [
+    FontAwesomeModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
